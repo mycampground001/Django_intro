dbc5221d1605bbe23d849488b50a08a7742b17e4
jeongSangyoung <mycampground@daum.net>
2019-02-11 06:37:02 +0000

    07_form_post'

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ import random
 
 def index(request):
     return render(request,'index.html')
-    
+
 def dinner(request):
     box = ['치킨','메뚜기','개구리','롯데리아']
     dinner = random.choice(box)
@@ -13,18 +13,26 @@ def dinner(request):
     # 1) request, 2) template 파일(html)
     # render 선택인자
     # 3) dictionary : 템플릿에서 쓸 변수 값을 정의
-    
+
     return render(request, 'dinner.html', {'dinner':dinner,'box':box})
     # zai flask return ('dinner.html',~~)
     # template은 기본적으로 문법이 jinja2랑 같은데, 장고에서는 DTL을 쓴다.
     # Django template Language
-    
+
 def cube(request, num):
     return render(request,'cube.html',{'num':num,'res':num**3})
-    
+
 def ping(request):
     return render(request,'ping.html')
-    
+
 def pong(request):
     msg = request.GET.get('message')
     return render(request, 'pong.html',{'msg':msg})
+
+def user_new(request):
+    return render(request, 'user_new.html')
+    
+def user_read(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    return render(request, 'user_read.html',{'username':username,'password':password})
